0af2294b86b4d12384b9eb993825444085dbec79
Adam Moniz <adamgrm9@gmail.com>
2021-09-07 18:12:06 -0400

    Add project files.

---
+++ MonizA_TicTacToe/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace MonizA_TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 39);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(112, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 39);
+            this.label2.TabIndex = 1;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(177, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 39);
+            this.label3.TabIndex = 2;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 39);
+            this.label4.TabIndex = 3;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(112, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 39);
+            this.label5.TabIndex = 4;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(177, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 39);
+            this.label6.TabIndex = 5;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(48, 155);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 39);
+            this.label7.TabIndex = 6;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(112, 155);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 39);
+            this.label8.TabIndex = 7;
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(177, 155);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 39);
+            this.label9.TabIndex = 8;
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winLabel
+            // 
+            this.winLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.winLabel.Location = new System.Drawing.Point(48, 209);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(168, 32);
+            this.winLabel.TabIndex = 9;
+            this.winLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(48, 253);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(75, 23);
+            this.newGameButton.TabIndex = 10;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(141, 253);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 302);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac_Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ MonizA_TicTacToe/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonizA_TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            //create a random number
+            Random rand = new Random();
+
+            //create the two-dimensional array
+            const int Rows = 3;
+            const int Cols = 3;
+            int[,] values = new int[Rows, Cols];
+
+            //Fill the array with Random numbers
+
+            for (int row = 0; row < Rows; row++)
+            {
+                for(int col = 0; col < Cols; col++)
+                {
+                    values[row, col] = rand.Next(2);
+                }
+            }
+
+            //variables to hold win results
+            int Xwins, Owins;
+
+            //display the random results
+            DisplayResults(values);
+
+            //collect the results in the variables
+            Owins = WinnerO(values);
+            Xwins = WinnerX(values);
+
+            //display winner
+            if (Owins > Xwins)
+                winLabel.Text = ("Player O is the winner!");
+            else if (Xwins > Owins)
+                winLabel.Text = ("Player X is the winner!");
+            else
+                winLabel.Text = ("Player O and Player X tied");
+        }
+
+        //function to display results on form
+        private void DisplayResults(int[,] results)
+        {
+            //get each label and display "X" or "O" depending on the value
+            //label1
+            if (results[0, 0] == 0)
+                label1.Text = ("O");
+            else
+                label1.Text = ("X");
+            //lable2
+            if (results[0, 1] == 0)
+                label2.Text = ("O");
+            else
+                label2.Text = ("X");
+            //label3
+            if (results[0, 2] == 0)
+                label3.Text = ("O");
+            else
+                label3.Text = ("X");
+            //label4
+            if (results[1, 0] == 0)
+                label4.Text = ("O");
+            else
+                label4.Text = ("X");
+            //label5
+            if (results[1, 1] == 0)
+                label5.Text = ("O");
+            else
+                label5.Text = ("X");
+            //label6
+            if (results[1, 2] == 0)
+                label6.Text = ("O");
+            else
+                label6.Text = ("X");
+            //label7
+            if (results[2, 0] == 0)
+                label7.Text = ("O");
+            else
+                label7.Text = ("X");
+            //label8
+            if (results[2, 1] == 0)
+                label8.Text = ("O");
+            else
+                label8.Text = ("X");
+            //label9
+            if (results[2, 2] == 0)
+                label9.Text = ("O");
+            else
+                label9.Text = ("X");
+        }
+
+        //this function will determine if the Player "O" won any rows, columns or diagonals
+        private int WinnerO(int [,] results)
+        {
+            const int Rows = 3, Cols = 3; //to use in loop
+            int Owins = 0; //to hold value to return
+            int total; //accumulator to sum rows, columns and diagonals to test if there are 3 in a row
+
+            //get the sum of the rows, if the sum is 0, then "O" wins that row
+            for (int row = 0; row < Rows; row++)
+            {
+                //set the accumulator to 0
+                total = 0;
+                //total the row
+                for (int col = 0; col < Cols; col++)
+                    total += results[row, col];
+
+                //add to Owins if results equal 0
+                if (total == 0)
+                    Owins++;
+            }
+
+            //get the sum of the columns, of the sum is 0, then "O" wins that column
+            for (int col = 0; col < Cols; col++)
+            {
+                //set accumulator to 0
+                total = 0;
+                //total the column
+                for (int row = 0; row < Rows; row++)
+                    total += results[row, col];
+
+                //add to Owins if results equal 0
+                if (total == 0)
+                    Owins++;
+            }
+
+            //test the diagonals for a win
+            if (results[0, 0] + results[1, 1] + results[2, 2] == 0)
+                Owins++;
+            if (results[0, 2] + results[1, 1] + results[2, 0] == 0)
+                Owins++;
+            //return Owins
+            return Owins;
+        }
+
+        //this function will determine if the Player "X" won any rows, columns or diagonals
+        private int WinnerX(int[,] results)
+        {
+            const int Rows = 3, Cols = 3; //to use in loop
+            int Xwins = 0; //to hold value to return
+            int total; //accumulator to sum rows, columns and diagonals to test if there are 3 in a row
+
+            //get the sum of the rows, if the sum is 3, then "X" wins that row
+            for (int row = 0; row < Rows; row++)
+            {
+                //set the accumulator to 0
+                total = 0;
+                //total the row
+                for (int col = 0; col < Cols; col++)
+                    total += results[row, col];
+
+                //add to Xwins if results equal 3
+                if (total == 3)
+                    Xwins++;
+            }
+
+            //get the sum of the columns, of the sum is 3, then "X" wins that column
+            for (int col = 0; col < Cols; col++)
+            {
+                //set accumulator to 0
+                total = 0;
+                //total the column
+                for (int row = 0; row < Rows; row++)
+                    total += results[row, col];
+
+                //add to Xwins if results equal 3
+                if (total == 3)
+                    Xwins++;
+            }
+
+            //test the diagonals for a win
+            if (results[0, 0] + results[1, 1] + results[2, 2] == 3)
+                Xwins++;
+            if (results[0, 2] + results[1, 1] + results[2, 0] == 3)
+                Xwins++;
+            //return Xwins
+            return Xwins;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close form
+            this.Close();
+        }
+    }
+}
